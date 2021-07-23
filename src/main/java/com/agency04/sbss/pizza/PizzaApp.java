fd05d728afc1b5a76c1e957e34608b3bfa2d12f6
Aida <aida.omanovic7@gmail.com>
2021-07-23 21:15:22 +0200

    Solve week-1 part 1

--- src/main/java/com/agency04/sbss/pizza/PizzaApp.java
+++ src/main/java/com/agency04/sbss/pizza/PizzaApp.java
@@ -1,9 +1,36 @@
 package com.agency04.sbss.pizza;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.sql.SQLOutput;
+
 public class PizzaApp {
-
     public static void main(String[] args) {
-        // Add some pizza code
-    }
 
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("spring-context.xml");
+
+        // pizzas
+        Pizza theDiavolaPizza = context.getBean("myDiavolaPizza", DiavolaPizza.class);
+        Pizza theTricolorePizza = context.getBean("myTricolorePizza", TricolorePizza.class);
+
+        // deliveries - testing
+        PizzaDeliveryService theFirstDelivery =
+                context.getBean("myFirstDelivery", PizzaDeliveryService.class);
+
+        System.out.println("Testing first pizzeria");
+        System.out.println(theFirstDelivery.orderPizza(theDiavolaPizza));
+        System.out.println(theFirstDelivery.orderPizza(theTricolorePizza));
+
+        System.out.println("\n");
+
+        PizzaDeliveryService theSecondDelivery =
+                context.getBean("mySecondDelivery", PizzaDeliveryService.class);
+
+        System.out.println("Testing second pizzeria");
+        System.out.println(theSecondDelivery.orderPizza(theDiavolaPizza));
+        System.out.println(theSecondDelivery.orderPizza(theTricolorePizza));
+
+        context.close();
+    }
 }
